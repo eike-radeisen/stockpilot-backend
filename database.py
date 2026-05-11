cbb9ca4ee8bc4e8f2e4a2eee32e19ba6948643a4
eike radeisen <eike.radeisen@bgr.de>
2026-05-11 11:07:38 +0200

    switch to postgres

--- database.py
+++ database.py
@@ -4,10 +4,14 @@ from sqlalchemy.orm import declarative_base, sessionmaker
 from dotenv import load_dotenv
 
 load_dotenv()
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./stockpilot.db")
 
-connect_args = {"check_same_thread": False} if DATABASE_URL.startswith("sqlite") else {}
-engine = create_engine(DATABASE_URL, connect_args=connect_args)
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True,  # wichtig für Render + Neon
+)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
